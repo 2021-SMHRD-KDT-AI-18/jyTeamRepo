a9c696f5ac9680935ed02b3bfe4a979e8a883b62
smhrd <smhrd@121.147.0.147>
2024-01-25 11:38:24 +0900

    다은씨 충돌 해결과정 선생님 설명 들은 후, 자영 다섯번째 커밋 완료!!

--- GitTest01/src/Ex01.java
+++ GitTest01/src/Ex01.java
@@ -3,10 +3,11 @@ public class Ex01 {
 
 	public static void main(String[] args) {
 
-		System.out.println("팀장 첫번째 커밋!!!!");
-		System.out.println("팀원1 두번째 커밋!!!");
-		System.out.println("팀원2 세번째 커밋!!!");
-		System.out.println("팀원3 네번째 커밋!!!");
+		System.out.println("팀장(자영) 첫번째 커밋!!!!");
+		System.out.println("팀원1(대길) 두번째 커밋!!!");
+		System.out.println("팀원2(현빈) 세번째 커밋!!!");
+		System.out.println("팀원3(민지) 네번째 커밋!!!");
+		System.out.println("팀원4(자영) 다섯번째 커밋!");
 		
 	}
 
