7b9f8ee17e181a927a11529446a73ab83f038d04
smhrd <smhrd@59.0.124.189>
2024-01-25 11:46:19 +0900

    여섯번째로 대길 커밋 완료

--- GitTest01/src/Ex01.java
+++ GitTest01/src/Ex01.java
@@ -8,6 +8,7 @@ public class Ex01 {
 		System.out.println("팀원2(현빈) 세번째 커밋!!!");
 		System.out.println("팀원3(민지) 네번째 커밋!!!");
 		System.out.println("팀원4(자영) 다섯번째 커밋!");
+		System.out.println("팀원1(대길) 여섯번째 커밋!");
 		
 	}
 
