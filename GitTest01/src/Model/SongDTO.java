d616829af7e6d00c9163613170522b8c8c9a40c1
smhrd <smhrd@121.147.0.147>
2024-01-26 16:13:13 +0900

    SongDAO에 노래재생 기능 추가, SongDTO에 노래 정보(곡제목,가수,노래경로)필드 추가 getter메소드 추가, Main에 임시로 노래 재생(문제출제), 다시듣기, 포기 시 다시듣기 기능 추가(외부파일 player.jar파일 설치 필요)

--- GitTest01/src/Model/SongDTO.java
+++ GitTest01/src/Model/SongDTO.java
@@ -1,5 +1,33 @@
 package Model;
 
 public class SongDTO {
-
+	//곡정보 필드
+	private String title = ""; 
+	private String singer = ""; 
+	private String path = ""; 
+	
+	
+	//곡정보  getter method
+	public String getTitle() {
+		return title;
+	}
+	public String getSinger() {
+		return singer;
+	}
+	public String getPath() {
+		return path;
+	}
+	
+	
+	// 곡정보 생성자
+	public SongDTO (String singer, String title, String path) {
+		this.singer = singer;
+		this.title = title;
+		this.path = path;
+	}	
+	
+	// 기본생성자
+	public SongDTO () {
+		
+	}
 }
