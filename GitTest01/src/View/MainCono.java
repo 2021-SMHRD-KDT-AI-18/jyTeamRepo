370449144a1ba2d1633d5ea82f33533a780e0d8d
smhrd <smhrd@59.0.234.65>
2024-01-29 12:17:52 +0900

    풀 하기전 커밋

--- GitTest01/src/View/MainCono.java
+++ GitTest01/src/View/MainCono.java
@@ -37,26 +37,113 @@ public class MainCono {
 		int nowCoin = 0;
 		
 		while(true) {
-			System.out.println("[1] 로그인 \t[2] 회원가입 \t[3] 랭킹 보기 \t[4] 게임시작하기 \t[5] 종료 하기");
+			System.out.println("[1] 게임시작 \t[2] 랭킹 보기 \t[3] 종료 하기");
 			choice = sc.nextInt();
-			if(choice == 1) {
-				System.out.println("로그인 화면");
-				System.out.print("ID 입력 : ");
-				String id = sc.next();
-				System.out.print("PW 입력 :");
-				String pw = sc.next();
+			
+			if(choice == 1) { //게임시작
+			
+			System.out.println("[1] 로그인 \t[2] 회원가입");
+			
+			//1번 게임 시작 - 로그인 
+			
+
+			choice = sc.nextInt();
+			
+			
+			
+				if(choice == 1) {
+					System.out.println("로그인 화면");
+					System.out.print("ID 입력 : ");
+					String id = sc.next();
+					System.out.print("PW 입력 :");
+					String pw = sc.next();
+					
+					UserDTO info = controller.login(id,pw);
 				
-				UserDTO info = controller.login(id,pw);
 				
 				if(info != null) {
 					System.out.println(" 환영합니다 ");
 					
 				}else {
+					if(info != null) {
+						System.out.println(" 환영합니다 ");
+					
+					
+						//로그인 성공 시 코인 투입 화면
+						while(true) {
+							System.out.print("코인을 투입해주세요(코인투입 : 1,게임시작 : 0)  : " );
+							int inputCoin;
+							inputCoin = sc.nextInt();
+							if(inputCoin == 1 && nowCoin<=10) {
+								nowCoin++;
+								//동전 효과음
+								songDao.soundCoinPlay();
+								System.out.println("현재 투입 코인 : " + nowCoin);
+							}else if(inputCoin == 1 && nowCoin == 10) {
+								System.out.println("게임을 시작합니다.");
+								break;
+							}else if (inputCoin == 0 && nowCoin <= 10 && nowCoin >= 1){
+								System.out.println("게임을 시작합니다.");
+								break;
+							}else if(inputCoin == 0 && nowCoin == 0 ) {
+								System.out.println("코인을 투입해주세요.(현재보유코인 : 0)");
+							}
+							else {
+								System.out.println("올바른 코인을 투입해주세요(1 또는 0 입력)");
+							}
+							
+							
+							// 코인 입력 화면 벗어나기
+							if(nowCoin == 10) {
+								System.out.println("게임을 시작합니다.(최대 코인 10개 투입)");
+								break;
+							}
+						}
+						
+						
+						//문제 출제
+						//노래재생
+						System.out.println("노래재생을 원하시면 start를 입력해주세요");
+						String start = sc.next();
+						if(start.equals("start")) {
+						songDao.musicPlay(songDto);
+						}
+						//다시듣기(1번입력시)
+						System.out.println("[1]정답입력\t[2]힌트\t[3]다시 듣기\t[4] 포기");
+						int num2 = sc.nextInt();
+						if(num2 ==3) {
+							songDao.musicPlay(songDto);
+						}else if(num2 ==4){
+							//포기 시 다시 듣기
+							songDao.musicPlay(songDto);
+							
+							//포기 시 정답 출력
+							songDao.answerOpen(songDto);
+						}
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						
+
+					
+
+					
+				}else { //로그인 실패
 					System.out.println("회원정보를 찾을 수 없습니다. 회원가입을 먼저 진행해주세요.");
 					
-					
+				}	
 				}
-				
+				}
 			}else if(choice == 2) {
 				System.out.println("회원 등록 화면");
 				System.out.print("ID 입력 : ");
@@ -72,11 +159,18 @@ public class MainCono {
 				
 				if(cnt > 0) {
 					System.out.println("회원등록 성공");
+					
+					
+					
 				}else {
 					System.out.println("회원등록 실패");
 				}
 				
-			}else if(choice == 3) {
+			}
+			
+			
+				
+			else if(choice == 2) { //랭킹보기
 				System.out.println("===== 랭킹 보기 =====");
 				ArrayList<UserDTO> list = controller.userlist();
 				
@@ -95,45 +189,10 @@ public class MainCono {
 			
 			
 		
-		
-		
-		
-		
-		
-		//문제 출제
-		//노래재생
-		System.out.println("노래재생을 원하시면 1번을 눌러주세요(시끄러워서 임시로 만들었어요)");
-		int num1 = sc.nextInt();
-		if(num1==1) {
-		songDao.musicPlay(songDto);
-		}
-		//다시듣기(1번입력시)
-		System.out.println("[1]정답입력\t[2]힌트\t[3]다시 듣기\t[4] 포기");
-		int num2 = sc.nextInt();
-		if(num2 ==3) {
-			songDao.musicPlay(songDto);
-		}else if(num2 ==4){
-			//포기 시 다시 듣기
-			songDao.musicPlay(songDto);
 			
-			//포기 시 정답 출력
-			songDao.answerOpen(songDto);
-		}
-		
-		
-		
-			else if(choice == 4) {
-				System.out.print("코인을 투입해주세요 : " );
-				int inputCoin = sc.nextInt();
-				
-				if(inputCoin == 1) {
-					nowCoin++;
-					System.out.println("현재 투입 코인 : " + nowCoin);
-				}
-						
-						
-				
-			}else if(choice == 5) {
+			
+			
+		else if(choice == 3) { //게임종료
 				break;
 			}
 		
@@ -141,5 +200,6 @@ public class MainCono {
 		sc.close();
 		
 	}
-
 }
+
+
